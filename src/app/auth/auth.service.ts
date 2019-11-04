3ddda9a351d2c63a242a62174594bfa7a8a4cfc5
Swaraj <swaraj.dehuri@gmail.com>
2019-11-04 19:17:32 +0530

    Auth Guard Added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,10 +1,17 @@
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
+@Injectable()
 export class AuthService {
     private user: User;
     authChange = new Subject<boolean>();
+
+    constructor(private router: Router) {
+
+    }
 
     registerUser(authData: AuthData) {
         this.user = {
@@ -12,6 +19,7 @@ export class AuthService {
             userId: Math.round(Math.random() * 10000).toString()
         };
         this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     login(authData: AuthData) {
@@ -20,11 +28,13 @@ export class AuthService {
             userId: Math.round(Math.random() * 10000).toString()
         };
         this.authChange.next(true);
+        this.authSuccessfully();
     }
 
     logout() {
         this.user = null;
         this.authChange.next(false);
+        this.router.navigate(['/login']);
     }
 
     getUser() {
@@ -33,6 +43,11 @@ export class AuthService {
 
     isAuth() {
         return this.user != null;
+    }
+
+    private authSuccessfully() {
+        this.authChange.next(true);
+        this.router.navigate(['/training']);
     }
 
 }
